04f3ace9316278010ed3e1ac03b4e7e27bd131fc
marijnkj <116631228+marijnkj@users.noreply.github.com>
2023-10-10 12:08:11 +0200

    Update app.R

--- app.R
+++ app.R
@@ -7,10 +7,11 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput("assembly_year", h3("Choose assembly year"),
                   choices=fun_get_assembly_year_options(), selected=fun_get_assembly_year_options()[1]),
-      textInput("beteckning", h3("Choose beteckning"), value="AU10"),
-      textInput("agenda_item", h3("Choose agenda point"), value="2"),
+      uiOutput("utskott_choice"),
+      uiOutput("beteckning_choice"),
+      textInput("agenda_item", h3("Choose agenda point"), value="1"),
       selectInput("filter", h3("Choose filter"),
-                             choices=list("Gender"="Gender", "Region"="Region", "Party"="Party"), selected="Gender")
+                  choices=list("Gender"="Gender", "Region"="Region", "Party"="Party"), selected="Gender"),
     ),
     mainPanel(
       plotlyOutput("bar_plot")
@@ -19,6 +20,16 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  output$utskott_choice <- renderUI({
+    selectInput("utskott", h3("Select utskott"),
+                choices=get_utskott(input$assembly_year))
+  })
+  
+  output$beteckning_choice <- renderUI({
+    selectInput("beteckning", h3("Select beteckning"),
+                choices=get_titlar(input$assembly_year, input$utskott))
+  })
+  
   output$bar_plot <- renderPlotly({
     df <- fun_fetch_data(input$assembly_year, input$beteckning, input$agenda_item)
     fun_bar_chart(df, input$filter)})
